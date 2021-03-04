893edce9983d277a66c84b5f1d829f19d4579177
Afonso Garcia <afonso.f.garcia@cern.ch>
2021-03-04 17:33:47 +0100

    Formatting

--- src/components/bcgroup/BcGroup.tsx
+++ src/components/bcgroup/BcGroup.tsx
@@ -3,18 +3,25 @@ import { ExpandMore } from '@material-ui/icons'
 import React, { PropsWithChildren } from 'react'
 import BcGrid from '../bcgrid/BcGrid'
 
-function BcGroup({id, title, expanded, setExpanded, children}: PropsWithChildren<{id: string, title: string, expanded: string[], setExpanded: (expanded: string[]) => void}>) {
+function BcGroup({
+  id,
+  title,
+  expanded,
+  setExpanded,
+  children,
+}: PropsWithChildren<{ id: string; title: string; expanded: string[]; setExpanded: (expanded: string[]) => void }>) {
   return (
-    <Accordion expanded={expanded.includes(id)} onChange={(_event, isExpanded) => isExpanded ? setExpanded([id, ...expanded]) : setExpanded(expanded.filter(value => value !== id)) }>
+    <Accordion
+      expanded={expanded.includes(id)}
+      onChange={(_event, isExpanded) =>
+        isExpanded ? setExpanded([id, ...expanded]) : setExpanded(expanded.filter(value => value !== id))
+      }
+    >
       <AccordionSummary expandIcon={<ExpandMore />} aria-controls={id.concat('-content')} id={id.concat('-header')}>
-        <Typography>
-          {title}
-        </Typography>
+        <Typography>{title}</Typography>
       </AccordionSummary>
       <AccordionDetails id={id.concat('-content')}>
-        <BcGrid>
-          {children}
-        </BcGrid>
+        <BcGrid>{children}</BcGrid>
       </AccordionDetails>
     </Accordion>
   )
--- src/components/bcinput/BcInput.tsx
+++ src/components/bcinput/BcInput.tsx
@@ -1,19 +1,44 @@
-import { Grid, FormLabel, Hidden, FormHelperText, Drawer, createMuiTheme, ThemeProvider, IconButton } from "@material-ui/core"
-import HelpIcon from "@material-ui/icons/Help"
-import React, { ReactElement, ReactNode, useState } from "react"
+import {
+  Grid,
+  FormLabel,
+  Hidden,
+  FormHelperText,
+  Drawer,
+  createMuiTheme,
+  ThemeProvider,
+  IconButton,
+} from '@material-ui/core'
+import HelpIcon from '@material-ui/icons/Help'
+import React, { ReactElement, ReactNode, useState } from 'react'
 import styles from './BcInput.module.css'
 
-function Help({mobile = false, onClick}: {mobile?: boolean, onClick: () => void}) {
+function Help({ mobile = false, onClick }: { mobile?: boolean; onClick: () => void }) {
   return (
     <Hidden smDown={!mobile} mdUp={mobile}>
       <IconButton onClick={onClick} className={styles.HelpIcon}>
-        <HelpIcon/>
+        <HelpIcon />
       </IconButton>
     </Hidden>
   )
 }
 
-function BcInputWrapper({id, label, hint, help, fullWidth = false, inGrid = false, children} : {id: string, label: string, help?: ReactNode, hint?: string, fullWidth?: boolean, inGrid?: boolean, children: ReactElement}): JSX.Element {
+function BcInputWrapper({
+  id,
+  label,
+  hint,
+  help,
+  fullWidth = false,
+  inGrid = false,
+  children,
+}: {
+  id: string
+  label: string
+  help?: ReactNode
+  hint?: string
+  fullWidth?: boolean
+  inGrid?: boolean
+  children: ReactElement
+}): JSX.Element {
   const theme = createMuiTheme({
     overrides: {
       MuiFormControl: {
@@ -25,37 +50,39 @@ function BcInputWrapper({id, label, hint, help, fullWidth = false, inGrid = fals
   })
 
   const [isOpen, setOpen] = useState<boolean>(false)
-    return (
-      <ThemeProvider theme={theme}>
-        <Grid container direction="row" className={inGrid ? styles.MarginMiddle : ""}>
-          <Grid item xs={12} md={3}>
-            <Grid container alignItems="center" className={styles.NoWrap}>
-              <FormLabel htmlFor={id} className={styles.LabelPadding}>{label}</FormLabel>
-              {help && <Help onClick={() => setOpen(true)} />}
-            </Grid>
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container direction="row" className={inGrid ? styles.MarginMiddle : ''}>
+        <Grid item xs={12} md={3}>
+          <Grid container alignItems="center" className={styles.NoWrap}>
+            <FormLabel htmlFor={id} className={styles.LabelPadding}>
+              {label}
+            </FormLabel>
+            {help && <Help onClick={() => setOpen(true)} />}
           </Grid>
-          <Grid item xs={12} md={9} container alignItems="center">
-            <div className={fullWidth ? styles.FullScreenInput : ''}>
-              {React.cloneElement(children, {fullWidth, id, margin: 'dense', variant: 'outlined'})}
-            </div>
-            {help && <Help mobile onClick={() => setOpen(true)} />}
-          </Grid>
-          {hint && (
-            <>
-              <Hidden smDown>
-                <Grid item xs={3}></Grid>
-              </Hidden>
-              <Grid item xs={12} md={9}>
-                <FormHelperText id={id.concat('Hint')}>{hint}</FormHelperText>
-              </Grid>
-            </>
-          )}
         </Grid>
-        <Drawer anchor="bottom" open={isOpen} onClose={() => setOpen(false)}>
-          {help}
-        </Drawer>
-      </ThemeProvider>
-    )
+        <Grid item xs={12} md={9} container alignItems="center">
+          <div className={fullWidth ? styles.FullScreenInput : ''}>
+            {React.cloneElement(children, { fullWidth, id, margin: 'dense', variant: 'outlined' })}
+          </div>
+          {help && <Help mobile onClick={() => setOpen(true)} />}
+        </Grid>
+        {hint && (
+          <>
+            <Hidden smDown>
+              <Grid item xs={3}></Grid>
+            </Hidden>
+            <Grid item xs={12} md={9}>
+              <FormHelperText id={id.concat('Hint')}>{hint}</FormHelperText>
+            </Grid>
+          </>
+        )}
+      </Grid>
+      <Drawer anchor="bottom" open={isOpen} onClose={() => setOpen(false)}>
+        {help}
+      </Drawer>
+    </ThemeProvider>
+  )
 }
 
 export default BcInputWrapper
